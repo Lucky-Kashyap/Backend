f0bfd486f77525c95f6e790f5f8bf654f63c3580
Lucky <luckykashyap3430@gmail.com>
2023-08-26 15:12:15 +0530

    factory function

--- Misleneous/FrontEnd/app.js
+++ Misleneous/FrontEnd/app.js
@@ -1,28 +1,54 @@
 // oops
 
-const st1  ={
-    name:"adam",
-    age:25,
-    marks:95,
-    getMarks: function(){
-        return this.marks;
+// const st1  ={
+//     name:"adam",
+//     age:25,
+//     marks:95,
+//     getMarks: function(){
+//         return this.marks;
+//     }
+// }
+
+// const st2  ={
+//     name:"eve",
+//     age:25,
+    // marks:99,
+    // getMarks: function(){
+//         return this.marks;
+//     }
+// }
+
+// const st3  ={
+//     name:"casey",
+//     age:23,
+//     marks:85,
+//     getMarks: function(){
+//         return this.marks;
+//     }
+// }
+
+
+
+// factory function
+
+function PersonMaker(name,age){
+    const person = {
+        name:name,
+        age:age,
+        talk(){
+            console.log(`Hi my name is ${this.name}`);
+        }
     }
+
+    return person;
 }
 
-const st2  ={
-    name:"eve",
-    age:25,
-    marks:99,
-    getMarks: function(){
-        return this.marks;
-    }
-}
 
-const st3  ={
-    name:"casey",
-    age:23,
-    marks:85,
-    getMarks: function(){
-        return this.marks;
-    }
-}
+
+
+let p1 = PersonMaker('adam',25);
+
+console.log(p1);
+
+
+p1.talk();
