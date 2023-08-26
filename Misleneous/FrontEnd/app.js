5554642aa76ef7a8b34522c9c83dd27aa31dc809
Lucky <luckykashyap3430@gmail.com>
2023-08-26 16:15:51 +0530

    inheritance

--- Misleneous/FrontEnd/app.js
+++ Misleneous/FrontEnd/app.js
@@ -60,20 +60,20 @@
 
 // class
 
-class Person {
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-    }
+// class Person {
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
 
-    talk(){
-        console.log(`Hi, my name is ${this.name}`);
-    }
-}
+//     talk(){
+//         console.log(`Hi, my name is ${this.name}`);
+//     }
+// }
 
 
-let p1 = new Person('adam',23);
-let p2 = new Person('eve',25);
+// let p1 = new Person('adam',23);
+// let p2 = new Person('eve',25);
 
 
 
@@ -85,3 +85,80 @@ let p2 = new Person('eve',25);
 
 
 // p1.talk();
+
+
+// Inheritance
+
+class Person{
+    constructor(name,age){
+        console.log('Person class constructor');
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`Hi, I am ${this.name}`);
+    }
+}
+
+
+class Student extends Person{
+    constructor(name,age,marks){
+        console.log('Student class constructor');
+       super(name,age); // Parent class constructor being called
+        this.marks = marks;
+    }
+    // talk(){
+    //     console.log(`Hi, I am ${this.name}`);
+    // }
+}
+
+// let stu1 = new Student('adam',23,56);
+
+
+
+class Teacher extends Person{
+    constructor(name,age,subject){
+        console.log('Teacher class constructor');
+        super(name,age);
+        this.subject = subject;
+    }
+}
+
+let t1 = new Teacher('eve',32,'English');
+
+class Mammal {
+    constructor(name){
+        this.name=name;
+        this.type='warm-blooded';
+    }
+    eat(){
+        console.log('I am eating');
+    }
+}
+
+class Dog extends Mammal{
+    constructor(name){
+        super(name);
+    }
+    bark(){
+        console.log('wooff....');
+    }
+    // overriding method
+
+    eat(){
+        console.log('Dog is eating');
+    }
+}
+
+class Cat extends Mammal{
+    constructor(name){
+        super(name);
+    }
+    cat(){
+        console.log('meow....');
+    }
+}
+
+let dog1 = new Dog('tuffie');
+
+console.log(dog1.name);
