b60fe6c0a354c3a2412eb79e3d0fb3e33fa4ae90
Lucky <luckykashyap3430@gmail.com>
2023-08-26 15:05:14 +0530

    oops concept

---
+++ Misleneous/FrontEnd/app.js
@@ -0,0 +1,28 @@
+// oops
+
+const st1  ={
+    name:"adam",
+    age:25,
+    marks:95,
+    getMarks: function(){
+        return this.marks;
+    }
+}
+
+const st2  ={
+    name:"eve",
+    age:25,
+    marks:99,
+    getMarks: function(){
+        return this.marks;
+    }
+}
+
+const st3  ={
+    name:"casey",
+    age:23,
+    marks:85,
+    getMarks: function(){
+        return this.marks;
+    }
+}
