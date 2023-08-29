aa77e25ba6413c6b7165464c8dae1c339242a446
Lucky <luckykashyap3430@gmail.com>
2023-08-29 17:04:36 +0530

    update content through patch request

--- Rest/index.js
+++ Rest/index.js
@@ -4,18 +4,21 @@ const app = express();
 
 const port = 8080;
 const path = require('path');
+const {v4:uuidv4} = require('uuid');
+
+
 
 let posts = [
     {
-        id:1,
+        id:uuidv4(),
         username:'lucky',
         content:'I love Coding'
     },
-    {   id:2,
+    {   id:uuidv4(),
         username:'apniduniya',
         content:'Hardwork is important'
     },
-    {   id:3,
+    {   id:uuidv4(),
         username:'ajay',
         content:'I  got my first internship'
     }
@@ -42,15 +45,43 @@ app.get('/posts/new',(req,res)=>{
 app.post('/posts',(req,res)=>{
     // console.log(req.body)
     let {username,content} = req.body;
+    let id = uuidv4();
 
-    posts.push({username,content});
+    posts.push({id,username,content});
     // res.send('post request working');
     // res.send('index.ejs',{posts})
 
     res.redirect('/posts');
 });
 
+app.get('/posts/:id',(req,res)=>{
+    let {id} = req.params;
+    // let {name} = req.query;
+    // console.log(name);
 
+    let post = posts.find((post)=>id==post.id)
+    // console.log(id);
+    // console.log(post);
+    // res.send('request working')
+    res.render('show.ejs',{post})
+})
+
+app.patch('/posts/:id',(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((post)=>id==post.id)
+    post.content = newContent;
+    console.log(post);
+    // console.log(id);
+    // console.log(newContent);
+    res.send('patch request');
+})
+// app.delete('/posts/:id',(req,res)=>{
+//     let {id} = req.params;
+
+//     const post = posts.filter((post)=>id!==post.id);
+//     res.render('show.ejs',{post});
+// })
 app.listen(port,()=>{
     console.log('Listening to port ');
 })
