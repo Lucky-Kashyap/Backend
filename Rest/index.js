755d2943b090b40e3519d47c4c73efddb5ff119a
Lucky <luckykashyap3430@gmail.com>
2023-08-29 15:34:42 +0530

    display post

--- Rest/index.js
+++ Rest/index.js
@@ -5,17 +5,33 @@ const app = express();
 const port = 8080;
 const path = require('path');
 
+let posts = [
+    {
+        username:'lucky',
+        content:'I love Coding'
+    },
+    {
+        username:'apniduniya',
+        content:'Hardwork is important'
+    },
+    {
+        username:'ajay',
+        content:'I  got my first internship'
+    }
+]
+
 app.use(express.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 
-app.set(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 
-app.get('/',(req,res)=>{
-    res.send('service working well..!')
+app.get('/posts',(req,res)=>{
+    // res.send('service working well..!')
+    res.render('index.ejs',{posts});
 })
 
 app.listen(port,()=>{
