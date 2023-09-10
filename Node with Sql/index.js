3900526108518c61d16da9749d0339db67badb32
Lucky <luckykashyap3430@gmail.com>
2023-09-10 15:51:51 +0530

    connection build using mysql2 package

--- Node with Sql/index.js
+++ Node with Sql/index.js
@@ -1,14 +1,46 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require('mysql2');
 
-let getRandomUser = () => {
-  return {
-    id: faker.datatype.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
 
-console.log(getRandomUser());
+
+
+
+
+// create the connection to database
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password:'rootlucky',
+});
+
+try{
+  connection.query("SHOW TABLES",(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+  });
+ 
+}
+catch(err){
+  console.log(err);
+}
+
+connection.end();
+
+// let getRandomUser = () => {
+//   return {
+//     id: faker.datatype.uuid(),
+//     username: faker.internet.userName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password(),
+//   };
+// };
+
+
+
+
+
+// console.log(getRandomUser());
 
 
