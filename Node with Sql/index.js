08924c59408be5f55a4a767c996cc2d86cf617f8
Lucky <luckykashyap3430@gmail.com>
2023-09-10 16:25:10 +0530

    insert user data uing sql query in node js

--- Node with Sql/index.js
+++ Node with Sql/index.js
@@ -15,16 +15,28 @@ const connection = mysql.createConnection({
   password:'rootlucky',
 });
 
-let q = "SHOW TABLES";
+// let q = "SHOW TABLES";
+// inserting new data
+
+// let q = "Insert into user (id,username,email,password) values (?,?,?,?)";
+
+let q = "Insert into user (id,username,email,password) values ?";
+
+let user = [
+  ["1231","1231_newuser","abc31@gmail.com","31abc"],
+  ["124","124_newuser","abc4@gmail.com","4abc"],
+  ["125","125_newuser","abc5@gmail.com","5abc"],
+  ["126","126_newuser","abc6@gmail.com","6abc"]
+];
 
 
 try{
-  connection.query(q,(err,result)=>{
+  connection.query(q,[user],(err,result)=>{
     if(err) throw err;
-    // console.log(result);
-    console.log(result.length);
-    console.log(result[0]);
-    console.log(result[1]);
+    console.log(result);
+    // console.log(result.length);
+    // console.log(result[0]);
+    // console.log(result[1]);
   });
  
 }
