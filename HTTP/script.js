cbbc547b9ac8cd3fba8fcc89846a2ed6f4a44f63
Lucky <luckykashyap3430@gmail.com>
2024-05-25 12:58:22 +0530

    basic routing

--- HTTP/script.js
+++ HTTP/script.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
+// var server = http.createServer(function (req, res) {
+// res.end("server runs successfully");
+
+//   res.end("server runs using nodemon");
+// });
+
+// server.listen(3000);
+
 var server = http.createServer(function (req, res) {
+  // console.log(req.url === "/");
   // res.end("server runs successfully");
 
-  res.end("server runs using nodemon");
+  if (req.url === "/") {
+    res.end("hey");
+  } else if (req.url === "/profile") {
+    res.end("profile page");
+  } else {
+    res.end("page not found");
+  }
 });
 
 server.listen(3000);
--- README.md
+++ README.md
@@ -81,3 +81,18 @@
       });
 
       server.listen(3000);
+
+- Routing
+
+      var server = http.createServer(function (req, res) {
+      // console.log(req.url === "/");
+      // res.end("server runs successfully");
+
+      if (req.url === "/") {
+      res.end("hey");
+      } else if (req.url === "/profile") {
+      res.end("profile page");
+      } else {
+      res.end("page not found");
+      }
+      });
