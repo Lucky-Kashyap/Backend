e504a6e706f1ec3d9c0547fd99667174e3046a8e
Lucky <luckykashyap3430@gmail.com>
2024-09-16 23:05:20 +0530

    more about CJS

--- module_system/app.js
+++ module_system/app.js
@@ -1,7 +1,13 @@
+const sum = require("./common js/app");
+
 // var num = 35;
 
-var num = 50;
+// var num = 50;
 
-global.num = 60;
+// global.num = 60;
 
-console.log(num);
+// console.log(num);
+
+console.log(sum(1, 2, 3, 4));
+
+// console.log(sum.sum(1, 2, 3, 3));
--- module_system/common js/app.js
+++ module_system/common js/app.js
@@ -1,9 +1,11 @@
-function sum(...nums) {
+module.exports = function sum(...nums) {
   return nums.reduce((acc, cur) => cur + acc);
-}
+};
 
-console.log(sum(10, 3, 4, 5, 6, 7, 7));
+// console.log(sum(10, 3, 4, 5, 6, 7, 7));
 
-console.log(sum(10, 3));
+// console.log(sum(10, 3));
 
-console.log(sum(10, 7, 7));
+// console.log(sum(10, 7, 7));
+
+// module.exports = { sum };
