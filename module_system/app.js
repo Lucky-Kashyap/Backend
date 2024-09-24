fc0890fab46019a8da6c3fb8f6a92a019ad9b546
Lucky <luckykashyap3430@gmail.com>
2024-09-24 21:26:55 +0530

    difference between module.exports vs exports

--- module_system/app.js
+++ module_system/app.js
@@ -1,4 +1,4 @@
-const sum = require("./common js/app");
+const { sum, product } = require("./common js/app");
 
 // var num = 35;
 
@@ -6,8 +6,35 @@ const sum = require("./common js/app");
 
 // global.num = 60;
 
+const user = {
+  name: "Lucky Kashyap",
+  age: 23,
+  address: {
+    city: "Agra",
+    state: "Uttar Pradesh",
+  },
+  hobbies: ["Teaching", "Coding", "TT"],
+};
+
+let address = user.address;
+
+address = {
+  pincode: 282006,
+  country: "India",
+};
+
+// address.pinCode = 282006;
+// address.country = "India";
+
 // console.log(num);
 
-console.log(sum(1, 2, 3, 4));
+// console.log(sum(1, 2, 3, 4));
+// console.log(product(1, 2, 3, 4));
+
+// console.log(sum, product);
 
 // console.log(sum.sum(1, 2, 3, 3));
+
+console.log(address);
+
+console.log(user.address);
--- module_system/common js/app.js
+++ module_system/common js/app.js
@@ -1,6 +1,18 @@
-module.exports = function sum(...nums) {
+// module.exports = function sum(...nums) {
+//   return nums.reduce((acc, cur) => cur + acc);
+// };
+
+// exports = function product(...nums) {
+//   return nums.reduce((acc, cur) => cur * acc);
+// };
+
+function sum(...nums) {
   return nums.reduce((acc, cur) => cur + acc);
-};
+}
+
+function product(...nums) {
+  return nums.reduce((acc, cur) => cur * acc);
+}
 
 // console.log(sum(10, 3, 4, 5, 6, 7, 7));
 
@@ -9,3 +21,13 @@ module.exports = function sum(...nums) {
 // console.log(sum(10, 7, 7));
 
 // module.exports = { sum };
+
+// console.log(module.exports === exports);
+
+// exports.sum = sum;
+// exports.product = product;
+
+let send = module.exports;
+
+send.sum = sum;
+send.product = product;
