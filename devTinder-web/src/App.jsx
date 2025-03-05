e02bac608efb53414f791e2b7fb4997c60d18b42
Lucky-Kashyap <luckykashyap3430@gmail.com>
2025-03-05 18:17:15 +0530

    fixed tailwind css loading issue

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return <div className="w-full h-full bg-zinc-700 text-white text-2xl">
+
+<h1>Navbar</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eligendi labore? Minima delectus quisquam, necessitatibus ab fugiat, consectetur assumenda dolorem praesentium aperiam, itaque quis provident. Minus tenetur eveniet ut neque!</p>
+  </div>;
 }
 
 export default App;
