08ec9561f7d33ab2386ccd55a91fecacd9141c7d
Lucky-Kashyap <luckykashyap3430@gmail.com>
2025-03-05 18:31:10 +0530

    navbar created

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
-  return <div className="w-full h-full bg-zinc-700 text-white text-2xl">
+import Navbar from "./components/Navbar";
 
-<h1>Navbar</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eligendi labore? Minima delectus quisquam, necessitatibus ab fugiat, consectetur assumenda dolorem praesentium aperiam, itaque quis provident. Minus tenetur eveniet ut neque!</p>
-  </div>;
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
