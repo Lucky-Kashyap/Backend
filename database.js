827a9fc740d9ea2ce4ec2dd02efaf3ef13bdb572
Lucky-Kashyap <luckykashyap3430@gmail.com>
2026-01-20 16:18:06 +0530

    Add MongoDB dependency and connection notes; create package.json and package-lock.json

--- database.js
+++ database.js
@@ -1,4 +1,14 @@
+require('mongodb');
+
 const URI = 'mongodb+srv://kashyapdivyanshu278_db_user:DaYrn8T0lCUzBk8R@namastelucky.zzum5gg.mongodb.net/';
 
 
+
+// Notes
+
+// Go to mongodb website
+// Create a free M0 cluster
+// Create a user
+// Get the connection string
+// Install Mongo DB compass
 
